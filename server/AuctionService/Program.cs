8dbc546baf807c58194067b273e1a3d1af3977b5
Victor <vi7org@hotmail.com>
2024-05-26 16:09:04 -0400

    Commit message:
    Implemented initial database setup with Entity Framework Core and PostgreSQL. Created Auction and Item entities with a one-to-one relationship and migration for the initial database schema. Configured the connection string for PostgreSQL in the appsettings.Development.json file. Additionally, updated docker-compose.yml for PostgreSQL container setup. Also, updated the program.cs file to configure the AuctionDbContext with PostgreSQL using the "DefaultConnection" string from the app configuration.

--- server/AuctionService/Program.cs
+++ server/AuctionService/Program.cs
@@ -1,7 +1,14 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+// Configures AuctionDbContext with PostgreSQL using the "DefaultConnection" string from app configuration.
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
