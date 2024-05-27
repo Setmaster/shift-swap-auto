be00a2b9b3aa7366a9d7a0b07b95196ad55f4cfa
Victor <vi7org@hotmail.com>
2024-05-27 11:56:50 -0400

    Added MongoDB integration to the SearchService. Added Item model.

--- server/SearchService/Program.cs
+++ server/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,5 +14,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Initialize the database
+await DB.InitAsync("SearchDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+// Create a text index on the Make, Model and Color fields
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
