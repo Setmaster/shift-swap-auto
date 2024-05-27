c13d090d15e4cdad6bb8f515a3118fa310a44274
Victor <vi7org@hotmail.com>
2024-05-27 13:52:27 -0400

    Added Polly policy to handle HTTP request retries in SearchService, also, moved the database initialization inside the application started callback for better error handling.

--- server/SearchService/Program.cs
+++ server/SearchService/Program.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -8,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -18,13 +21,37 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+// Registering a callback to be executed when the application has fully started
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
+    try
+    {
+        // Attempting to initialize the database
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+});
 
 app.Run();
+
+// This method defines and returns an asynchronous retry policy for HTTP requests.
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+{
+    // The policy is defined using extension methods provided by Polly's HttpPolicyExtensions
+    return HttpPolicyExtensions
+
+        // The HandleTransientHttpError method configures the policy to handle transient HTTP errors
+        // 5xx server errors and 408 request timeout errors
+        .HandleTransientHttpError()
+
+        // The OrResult method adds an additional condition to the policy
+        // In this case, it specifies that HTTP 404 Not Found responses should also be treated as transient errors
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+
+        // The WaitAndRetryForeverAsync method defines the retry behavior
+        // It specifies that the policy should retry indefinitely, with a delay of 3 seconds between each retry attempt
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+}
