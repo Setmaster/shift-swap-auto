72c0f499c08cc4eefad5177887cc1d477e374f07
Victor <vi7org@hotmail.com>
2024-05-26 20:34:30 -0400

    Added CreateAuction method in AuctionsController and updated AutoMapper profiles. The CreateAuction method maps the received DTO to an Auction entity, adds it to the database, and returns a mapped DTO. AutoMapper profiles were updated to support nested mappings from CreateAuctionDto to both Auction and Item entities.

--- server/AuctionService/Controllers/AuctionsController.cs
+++ server/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,7 +9,7 @@ namespace AuctionService.Controllers;
 
 [ApiController]
 [Route("api/auctions")]
-public class AuctionsController: ControllerBase
+public class AuctionsController : ControllerBase
 {
     private readonly AuctionDbContext _context; // Variable for database context
     private readonly IMapper _mapper; // Variable for AutoMapper
@@ -16,23 +17,24 @@ public class AuctionsController: ControllerBase
     // Constructor to initialize the database context and AutoMapper
     public AuctionsController(AuctionDbContext context, IMapper mapper)
     {
-        _context = context; 
+        _context = context;
         _mapper = mapper;
     }
-    
+
     [HttpGet]
     public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
     {
         // Fetching all auctions from the database, including related items, and ordering by item make
         var auctions = await _context.Auctions
-            .Include(x => x.Item) // This tells EF Core to include the related Item entity when querying the Auctions table, eager loading. Without this, the Item properties would not be loaded automatically (lazy loading) and would require a separate query to access 
+            .Include(x =>
+                x.Item) // This tells EF Core to include the related Item entity when querying the Auctions table, eager loading. Without this, the Item properties would not be loaded automatically (lazy loading) and would require a separate query to access 
             .OrderBy(x => x.Item.Make)
             .ToListAsync();
-            
+
         // Mapping the list of auctions to a list of AuctionDto objects and returning it
         return _mapper.Map<List<AuctionDto>>(auctions);
     }
-    
+
     [HttpGet("{id}")]
     public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
     {
@@ -40,14 +42,37 @@ public class AuctionsController: ControllerBase
         var auction = await _context.Auctions
             .Include(x => x.Item)
             .FirstOrDefaultAsync(x => x.Id == id);
-        
+
         // If no auction is found, return a 404 Not Found response
         if (auction == null)
         {
             return NotFound();
         }
-        
+
         // Mapping the auction to an AuctionDto object and returning it
         return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        // Mapping the auctionDto object to an Auction object
+        var auction = _mapper.Map<Auction>(auctionDto);
+
+        // TODO: add current user as seller
+        auction.Seller = "test";
+
+        // Adding the auction to the database
+        _context.Auctions.Add(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result)
+        {
+            return BadRequest("Could not save changes to the DB");
+        }
+
+        // Mapping the auction to an AuctionDto object and returning it 
+        return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
     }
 }
--- server/AuctionService/RequestHelpers/MappingProfiles.cs
+++ server/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -9,6 +9,7 @@ public class MappingProfiles : Profile
     public MappingProfiles()
     {
         // Configures a mapping from Auction entity to AuctionDto
+        // The properties from the nested Item entity are also mapped
         CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
        
         // Sets up a direct mapping from Item entity to AuctionDto
@@ -18,6 +19,11 @@ public class MappingProfiles : Profile
         // Specifically maps the entire CreateAuctionDto to the Item property of Auction
         CreateMap<CreateAuctionDto, Auction>()
             .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        
+        // Configures a mapping from CreateAuctionDto to Item entity
+        // This explicitly defines the mapping from CreateAuctionDto to Item, which is necessary to support the nested mapping
+        CreateMap<CreateAuctionDto, Item>();
     }
+    
     
 }
