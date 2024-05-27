7053d6a5cfda3e24de2cacf40cc30fd44573cee0
Victor <vi7org@hotmail.com>
2024-05-27 12:27:09 -0400

    Updated the SearchItems method in the SearchController to support pagination

--- server/SearchService/Controllers/SearchController.cs
+++ server/SearchService/Controllers/SearchController.cs
@@ -4,17 +4,16 @@ using SearchService.Models;
 
 namespace SearchService.Controllers
 {
-
     [ApiController]
     [Route("api/search")]
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumer = 1, int pageSize = 4)
         {
-            // Create a query for the Item collection in MongoDB
-            var query = DB.Find<Item>();
-            
+            // Create a paged search query for the 'Item' collection
+            var query = DB.PagedSearch<Item>();
+
             // Sort the query results in ascending order by the 'Make' field
             query.Sort(x => x.Ascending(a => a.Make));
 
@@ -23,12 +22,20 @@ namespace SearchService.Controllers
             {
                 query.Match(Search.Full, searchTerm).SortByTextScore();
             }
-            
+
+            // Set the page number and page size for the query
+            query.PageNumber(pageNumer).PageSize(pageSize);
+
             // Execute the query asynchronously and get the results
             var result = await query.ExecuteAsync();
 
-            // Return the query results to the client
-            return result;
+            // Return the query results to the client 
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
         }
     }
 }
