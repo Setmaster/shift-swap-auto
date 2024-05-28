f9de7d3098916587b132e5cf27a33606405504bd
Victor <vi7org@hotmail.com>
2024-05-28 18:39:16 -0400

    Added BidPlacedConsumer and AuctionFinishedConsumer classes to manage auction events

---
+++ server/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+// updates the current high bid for the auction
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming BidPlaced event");
+
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction.CurrentHighBid == null
+            || context.Message.BidStatus.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
