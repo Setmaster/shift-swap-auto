c619d7d131739053a69286ec1a034f6e212451ec
Victor <vi7org@hotmail.com>
2024-05-28 17:36:44 -0400

    Added IdentityServiceUrl and Enhanced ReverseProxy Routes and implemented JWT Authentication for Gateway Service

--- server/GatewayService/Program.cs
+++ server/GatewayService/Program.cs
@@ -1,10 +1,27 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(cfg=> {
+        cfg.Authority = builder.Configuration["IdentityServiceUrl"];
+        cfg.RequireHttpsMetadata = false;
+        
+        // Set the audience validation to false, as we are not using the audience claim
+        cfg.TokenValidationParameters.ValidateAudience = false;
+        
+        // Set the claim type to be used as the name identifier
+        cfg.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
