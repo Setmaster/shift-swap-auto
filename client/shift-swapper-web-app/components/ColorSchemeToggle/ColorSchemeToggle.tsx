4df00f1fdf7fc303e823a70eb53225510741d94b
Victor <vi7org@hotmail.com>
2024-05-30 14:12:27 -0400

    Change color scheme toggle into a switch

--- client/shift-swapper-web-app/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ client/shift-swapper-web-app/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,26 +1,52 @@
 'use client';
 
-import cx from 'clsx';
-import {ActionIcon, useMantineColorScheme, useComputedColorScheme, Group} from '@mantine/core';
-import {IconSun, IconMoon} from '@tabler/icons-react';
-import classes from './ColorSchemeToggle.module.css';
-import {useEffect, useState} from "react";
+import React, { useState, useEffect } from 'react';
+import { Switch, useMantineTheme, rem, Group } from '@mantine/core';
+import { IconSun, IconMoonStars } from '@tabler/icons-react';
+import { useMantineColorScheme, useComputedColorScheme } from '@mantine/core';
 
-export default function ColorSchemeToggle({justify = 'center'}) {
-    const {setColorScheme} = useMantineColorScheme();
-    const computedColorScheme = useComputedColorScheme('light', {getInitialValueInEffect: true});
-    
+export default function ColorSchemeToggle({ justify = 'center' }) {
+    const theme = useMantineTheme();
+    const { colorScheme, setColorScheme } = useMantineColorScheme();
+    const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
+    const [checked, setChecked] = useState(computedColorScheme === 'dark');
+
+    useEffect(() => {
+        setChecked(computedColorScheme === 'dark');
+    }, [computedColorScheme]);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const isChecked = event.currentTarget.checked;
+        setColorScheme(isChecked ? 'dark' : 'light');
+        setChecked(isChecked);
+    };
+
+    const sunIcon = (
+        <IconSun
+            style={{ width: rem(16), height: rem(16) }}
+            stroke={2.5}
+            color={theme.colors.yellow[4]}
+        />
+    );
+
+    const moonIcon = (
+        <IconMoonStars
+            style={{ width: rem(16), height: rem(16) }}
+            stroke={2.5}
+            color={theme.colors.blue[6]}
+        />
+    );
+
     return (
         <Group justify={justify}>
-            <ActionIcon
-                onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
-                variant="default"
-                size="xl"
-                aria-label="Toggle color scheme"
-            >
-                <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
-                <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
-            </ActionIcon>
+            <Switch
+                size="md"
+                color="dark.4"
+                checked={checked}
+                onChange={handleChange}
+                onLabel={sunIcon}
+                offLabel={moonIcon}
+            />
         </Group>
     );
 }
