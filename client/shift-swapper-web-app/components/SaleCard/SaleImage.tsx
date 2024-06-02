52cceda49551acc9f8105882a271c209dd57aa88
Victor <vi7org@hotmail.com>
2024-06-02 09:06:29 -0400

    Add loading state to images

--- client/shift-swapper-web-app/components/SaleCard/SaleImage.tsx
+++ client/shift-swapper-web-app/components/SaleCard/SaleImage.tsx
@@ -1,5 +1,9 @@
-﻿import Image from "next/image";
+﻿'use client';
+
+import Image from "next/image";
 import classes from './SaleImage.module.css';
+import {useState} from "react";
+import {LoadingOverlay} from "@mantine/core";
 
 
 type SaleImageData = {
@@ -21,7 +25,17 @@ type SaleImageProps = {
 };
 
 export default function SaleImage({data }: SaleImageProps) {
+    const [isLoading, setIsLoading] = useState(true);
+    
+    
     return (
+        <>
+        <LoadingOverlay 
+            visible={isLoading}
+            zIndex={1000}
+            overlayProps={{ radius: "sm", blur: 25 }}
+            loaderProps={{ color: 'red', type: 'bars' }}
+        />
         <Image
             className={classes.saleImage}
             src={data.imageUrl}
@@ -29,6 +43,8 @@ export default function SaleImage({data }: SaleImageProps) {
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             style={{ objectFit: 'cover', objectPosition: 'center' }}
+            onLoadingComplete={() => setIsLoading(false)}
         />
+        </>
     );
 }
