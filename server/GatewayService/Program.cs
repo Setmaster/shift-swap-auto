bdb27f4ce1550aa6c7859d68065ebc101be2ccb5
Victor <vi7org@hotmail.com>
2024-06-06 15:02:37 -0400

    Configure Gateway service for notification service

--- server/GatewayService/Program.cs
+++ server/GatewayService/Program.cs
@@ -6,22 +6,32 @@ builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(cfg=> {
+    .AddJwtBearer(cfg =>
+    {
         cfg.Authority = builder.Configuration["IdentityServiceUrl"];
         cfg.RequireHttpsMetadata = false;
-        
+
         // Set the audience validation to false, as we are not using the audience claim
         cfg.TokenValidationParameters.ValidateAudience = false;
-        
+
         // Set the claim type to be used as the name identifier
         cfg.TokenValidationParameters.NameClaimType = "username";
     });
 
+builder.Services.AddCors(cfg =>
+{
+    cfg.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
 var app = builder.Build();
 
-app.MapReverseProxy();
+app.UseCors();
 
+app.MapReverseProxy();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.Run();
