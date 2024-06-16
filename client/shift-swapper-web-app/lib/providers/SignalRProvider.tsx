dfb8efc69fc66b09543a1d3c852e44907d63033b
Victor <vi7org@hotmail.com>
2024-06-15 20:33:17 -0400

    Update notify api url

--- client/shift-swapper-web-app/lib/providers/SignalRProvider.tsx
+++ client/shift-swapper-web-app/lib/providers/SignalRProvider.tsx
@@ -1,31 +1,28 @@
 ﻿'use client';
 
-import React, {ReactNode, useEffect, useState} from "react";
-import {HubConnection, HubConnectionBuilder} from "@microsoft/signalr";
-import {useAuctionStore} from "@/lib/hooks/useAuctionStore";
-import {useBidStore} from "@/lib/hooks/useBidStore";
-import {User} from "next-auth";
-import {notifications} from "@mantine/notifications";
+import React, { ReactNode, useEffect, useState } from "react";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import { useAuctionStore } from "@/lib/hooks/useAuctionStore";
+import { useBidStore } from "@/lib/hooks/useBidStore";
+import { User } from "next-auth";
+import { notifications } from "@mantine/notifications";
 import AuctionCreatedToast from "@/components/Toasts/AuctionCreatedToast";
 import AuctionFinishedToast from "@/components/Toasts/AuctionFinishedToast";
-import {getAuction} from "@/lib/actions/auctionActions";
+import { getAuction } from "@/lib/actions/auctionActions";
 
 type SignalRProviderProps = {
     children: ReactNode;
     user: User | null;
 }
 
-export default function SignalRProvider({children, user}: SignalRProviderProps) {
+export default function SignalRProvider({ children, user }: SignalRProviderProps) {
     const [connection, setConnection] = useState<HubConnection | null>(null);
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
     const addAuction = useAuctionStore(state => state.addAuction);
     const removeAuction = useAuctionStore(state => state.removeAuction);
-    const apiURL= process.env.NODE_ENV === 'production'
-        ? 'https://api.shiftswap.com/notifications'
-        : process.env.NEXT_PUBLIC_NOTIFY_URL;
-    
-    
+    const apiURL = process.env.NOTIFY_API_URL;
+
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
             .withUrl(apiURL!)
@@ -52,13 +49,13 @@ export default function SignalRProvider({children, user}: SignalRProviderProps) 
                         if (user?.username !== auction.seller) {
                             notifications.show({
                                 message: (
-                                    <AuctionCreatedToast auction={auction}/>
+                                    <AuctionCreatedToast auction={auction} />
                                 ),
                             });
                         }
                         addAuction(auction);
                     });
-                    
+
                     connection.on('AuctionDeleted', (auctionDeleted: { id: string }) => {
                         removeAuction(auctionDeleted.id);
                     });
@@ -67,7 +64,7 @@ export default function SignalRProvider({children, user}: SignalRProviderProps) 
                         const auction = await getAuction(auctionFinished.auctionId);
                         notifications.show({
                             message: (
-                                <AuctionFinishedToast auction={auction} auctionFinished={auctionFinished}/>
+                                <AuctionFinishedToast auction={auction} auctionFinished={auctionFinished} />
                             ),
                         });
                     });
